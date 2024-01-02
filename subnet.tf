8933c79c5ff4b49d356ef8e2339f8a2fffbf7e34
emilymorgenbesser <morgenbesseremily@gmail.com>
2024-01-02 14:11:47 -0500

    adds count

--- subnet.tf
+++ subnet.tf
@@ -2,6 +2,7 @@ resource "aws_subnet" "public" {
     vpc_id = aws_vpc.main.id
     cidr_block = var.cidr[count.index]
     availability_zone = var.az[count.index]
+    count = 2
 
     tags = {
         Name = "public-sub"
